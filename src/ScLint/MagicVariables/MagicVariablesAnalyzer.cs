d54dbe0a64e999750345ae3d03d0120f786526d3
Sebastian Iskra <sebastian.iskra@outlook.com>
2020-07-27 13:19:20 +0200

    Nearly delivered magic strings rule

--- src/ScLint/MagicVariables/MagicVariablesAnalyzer.cs
+++ src/ScLint/MagicVariables/MagicVariablesAnalyzer.cs
@@ -2,16 +2,25 @@
 using Microsoft.CodeAnalysis.CSharp;
 using Microsoft.CodeAnalysis.Diagnostics;
 using System.Collections.Immutable;
-using System.Text.RegularExpressions;
+using System.Linq;
 
 namespace MagicVariables
 {
+    public enum InvestigatedTokens
+    {
+        String = SyntaxKind.StringLiteralToken,
+        Numeric = SyntaxKind.NumericLiteralToken,
+        Character = SyntaxKind.CharacterLiteralToken
+    }
+
     [DiagnosticAnalyzer(LanguageNames.CSharp)]
     public class MagicVariablesAnalyzer : DiagnosticAnalyzer
     {
         public const string DiagnosticId = "ScLint9";
-        public static string Title = "This might be a magic string. Consider moving this value to variable";
+        public static string Title = "This might be a magic string. Consider assigning this value to variable";
         private const string Category = "Declaration of magic variable";
+
+        private readonly SyntaxKind[] investigatedNodes = { SyntaxKind.EqualsExpression, SyntaxKind.NotEqualsExpression };
 
         private static DiagnosticDescriptor Rule = new DiagnosticDescriptor(DiagnosticId, Title, Title, Category, DiagnosticSeverity.Warning, isEnabledByDefault: true, description: Title);
 
@@ -19,18 +28,18 @@ namespace MagicVariables
 
         public override void Initialize(AnalysisContext context)
         {
-            context.RegisterSyntaxNodeAction(AnalyzeNode, SyntaxKind.InvocationExpression);
+            context.RegisterSyntaxNodeAction(AnalyzeNode, investigatedNodes);
         }
 
         private static void AnalyzeNode(SyntaxNodeAnalysisContext context)
         {
-            var nodeText = context.Node.GetText().ToString().ToLower();
-
-            Regex regEx = new Regex($@"database.(getitem|getrootitem|selectitems|selectitemsusingxpath|selectsingleitem|selectsingleitemusingxpath)\(");
-
-            if (regEx.Match(nodeText).Success)
+            var childTokens = context.Node.DescendantTokens().Where(x => x.IsKind(SyntaxKind.StringLiteralToken) || x.IsKind(SyntaxKind.NumericLiteralToken) || x.IsKind(SyntaxKind.CharacterLiteralToken));
+            if (context.Node.DescendantTokens().Any())
             {
-                context.ReportDiagnostic(Diagnostic.Create(Rule, context.Node.GetLocation()));
+                foreach (var childToken in childTokens)
+                {
+                    context.ReportDiagnostic(Diagnostic.Create(Rule, childToken.GetLocation()));
+                }
             }
         }
     }
